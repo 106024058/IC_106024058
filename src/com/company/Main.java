830cfdb699f4dbd166ea9fd0d21f543e96ff72ce
106024058 <nancy888227@yahoo.com.tw>
2017-09-25 15:07:42 +0800

    106024058

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("106024058");// write your code here
+	System.out.println("106024058");// write your code here
 
-    System.out.print("資傳系");}
+    System.out.println("資傳系");}
 }
